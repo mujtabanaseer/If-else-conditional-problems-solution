4845e5cbab0e92c6cedcfb1d92c9b7688c180bfd
unknown <=mujtabanaseer.pakistan@gmail.com>
2021-01-17 19:58:44 +0500

    new update

---
+++ assets/js/Index.js
@@ -0,0 +1,20 @@
+let p1 = document.getElementById('p1')
+let p2 = document.getElementById('p2')
+let p3 = document.getElementById('p3')
+let p4 = document.getElementById('p4')
+let p5 = document.getElementById('p5')
+let p6 = document.getElementById('p6')
+let p7 = document.getElementById('p7')
+let p8 = document.getElementById('p8')
+let p9 = document.getElementById('p9')
+//-------------------------------------------
+document.getElementById('img0').onclick=document.getElementById('img-p1')
+    function img1() {
+     document.getElementById('img-p1')
+}
+// p1.addEventListener('click', img1)
+$('#p1').click(function (){
+    alert();
+})
+
+
